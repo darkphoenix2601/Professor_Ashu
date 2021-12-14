3ccd2131a1e1b0f0a0c0f7b57637c7869337eee4
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 07:58:25 +0530

    Update dbfunctions.py

--- professor/utils/dbfunctions.py
+++ professor/utils/dbfunctions.py
@@ -25,7 +25,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from professer import db
+from professor import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
